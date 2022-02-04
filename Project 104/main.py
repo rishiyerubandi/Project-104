28f68dab331be2832f1a6590291fdf3e02925ce9
rishiyerubandi <76133122+rishiyerubandi@users.noreply.github.com>
2022-02-04 17:29:16 -0500

    Add files via upload

---
+++ Project 104/main.py
@@ -0,0 +1,65 @@
+import csv
+from collections import Counter
+
+with open("SOCR-HeightWeight.csv", newline = "") as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+new_data = []
+
+for i in range(len(file_data)):
+    num = file_data[i][1]
+    new_data.append(float(num))
+
+def mean():
+    length = len(new_data)
+    total = 0
+
+    for i in new_data:
+        total+=i
+
+    mean = total/length
+    print(mean)
+
+def median():
+    l = len(new_data)
+    new_data.sort()
+
+    if(1%2 == 0):
+        m1 = float(new_data[1//2])
+        m2 = float(new_data[1//2-1])
+        median = (m1 + m2) / 2
+    else:
+        median = float(new_data[1//2])
+    
+    print(median)
+
+def mode():
+    data = Counter(new_data)
+    ranges = {
+        "50-60": 0,
+        "60-70": 0,
+        "70-80": 0
+    }
+    for height, occurence in data.items():
+        if 50 < float(height) < 60:
+            ranges["50-60"] += occurence
+        elif 60 < float(height) < 70:
+            ranges["60-70"] += occurence
+        elif 70 < float(height) < 80:
+            ranges["70-80"] += occurence
+    
+    mode_range, mode_occurence = 0,0
+
+    for range, occurence in ranges.items():
+        if occurence > mode_occurence:
+            mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+    mode = float((mode_range[0] + mode_range[1]) / 2)
+    mode = int(mode)
+
+    print(mode)
+
+mean()
+median()
+mode()
